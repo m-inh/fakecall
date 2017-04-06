0b633494664fed56394016b7140d89115d0b79f6
minhnt58 <tienminh.uet@gmail.com>
2017-04-06 21:00:10 +0700

    revert after one commit

--- app/src/main/java/com/uet/fakecall/activity/FakeCallRingerActivity.java
+++ app/src/main/java/com/uet/fakecall/activity/FakeCallRingerActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -15,26 +14,20 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
 import android.os.Vibrator;
 import android.provider.CallLog;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.telephony.PhoneNumberUtils;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -50,14 +43,13 @@ public class FakeCallRingerActivity extends AppCompatActivity {
     private static final int MISSED_CALL_NOTIFICATION = 1002;
     private static final String DURATION = "duration";
     private static final int NO_ANSWER = 1;
+    private static final int COUNT_TIME_UPDATE = 2;
 
     private TextView callStatus;
     private TextView callDuration;
     private Button btnDeline;
     private Button btnAnswer;
     private Button btnEndCall;
-    private ImageView ivPhotoFake;
-    private Bitmap photo;
 
     private RelativeLayout mainLayout;
     private RelativeLayout callActionButtonsLayout;
@@ -83,33 +75,37 @@ public class FakeCallRingerActivity extends AppCompatActivity {
     private boolean isAnswer = false;
     private String callNumber;
     private String callName;
-    private Handler handler = new Handler();
-    private Handler handlerCall;
+
+    private Handler handlerCall = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case NO_ANSWER:
+                    FakeCallRingerActivity.this.finish();
+                    break;
+                case COUNT_TIME_UPDATE:
+                    String dur = msg.getData().getString("CURRENT_TIME");
+                    callDuration.setText(dur);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incomming_call);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         Window window = getWindow();
         PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
 
-
         callNumber = getContactNumber();
         callName = getContactName();
-        photo = getIntent().getParcelableExtra(FakeCallFragment.FAKE_PHOTO);
-        ivPhotoFake = (ImageView) findViewById(R.id.iv_photo_fake);
-        ivPhotoFake.setImageBitmap(photo);
 
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
-
-        final Animation ringExpandAnimation = AnimationUtils.loadAnimation(this, R.anim.ring_expand);
-
-        final Animation ringShrinkAnimation = AnimationUtils.loadAnimation(this, R.anim.ring_shrink);
-
-        //final Drawable bg2 = getDrawable(R.drawable.answered_bg);
 
         contentResolver = getContentResolver();
         resources = getResources();
@@ -119,16 +115,20 @@ public class FakeCallRingerActivity extends AppCompatActivity {
         currentRingerMode = audioManager.getRingerMode();
         currentRingerVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
         currentMediaVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
         callStatus = (TextView) findViewById(R.id.tv_call_status);
         callDuration = (TextView) findViewById(R.id.tv_call_duration);
-        callDuration.setVisibility(View.GONE);
+
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         duration = extras.getInt(DURATION);
 
+        callDuration.setVisibility(View.INVISIBLE);
+
         btnEndCall = (Button) findViewById(R.id.btn_deny_after_answer);
         btnEndCall.setVisibility(View.GONE);
+
         btnEndCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,47 +136,10 @@ public class FakeCallRingerActivity extends AppCompatActivity {
             }
         });
 
-        //// TODO: endcall after duration = 0
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!isAnswer && duration >= 0){
-                    duration = duration - 1000;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if(isAnswer){
-
-                }else {
-                    Message msg = new Message();
-                    msg.what = NO_ANSWER;
-                    msg.setTarget(handlerCall);
-                    msg.sendToTarget();
-                }
-
-
-            }
-        }).start();
-
-        handlerCall = new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case NO_ANSWER:
-                        secs = -1;
-                        FakeCallRingerActivity.this.finish();
-                        break;
-                }
-            }
-        };
         btnDeline = (Button) findViewById(R.id.btn_deline);
         btnDeline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                secs = -1;
                 finish();
             }
         });
@@ -186,28 +149,51 @@ public class FakeCallRingerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 callStatus.setText("");
+                callStatus.setVisibility(View.INVISIBLE);
                 btnAnswer.setVisibility(View.GONE);
                 btnDeline.setVisibility(View.GONE);
-                mainLayout.setBackgroundResource(R.mipmap.answered_bg);
                 btnEndCall.setVisibility(View.VISIBLE);
                 audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
                 callDuration.setVisibility(View.VISIBLE);
                 stopRinging();
 
                 wakeLock.acquire();
-                handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                long min = (secs % 3600) / 60;
-                                long seconds = secs % 60;
 
-                                String dur = String.format(Locale.US, "%02d:%02d", min, seconds);
-                                secs++;
-                                callDuration.setText(dur);
-                                handler.postDelayed(this, 1000);
+                //// endcall when duration <= 0
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (!isAnswer && duration >= 0) {
+                            duration = duration - 1000;
+                            long min = secs / 60;
+                            long seconds = secs % 60;
+                            secs++;
 
+                            String dur = String.format(Locale.US, "%02d:%02d", min, seconds);
+
+                            Message msg = new Message();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("CURRENT_TIME", dur);
+                            msg.setData(bundle);
+                            msg.what = COUNT_TIME_UPDATE;
+                            msg.setTarget(handlerCall);
+
+                            msg.sendToTarget();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
                             }
-                        }, 10);
+                        }
+
+                        if (!isAnswer){
+                            Message msg = new Message();
+                            msg.what = NO_ANSWER;
+                            msg.setTarget(handlerCall);
+                            msg.sendToTarget();
+                        }
+                    }
+                }).start();
 
             }
         });
@@ -227,8 +213,8 @@ public class FakeCallRingerActivity extends AppCompatActivity {
 
         muteAll();
 
-        TextView fakeName = (TextView)findViewById(R.id.tv_caller_name_fake_call_ring);
-        TextView fakeNumber = (TextView)findViewById(R.id.tv_number_fake_call_ring);
+        TextView fakeName = (TextView) findViewById(R.id.tv_caller_name_fake_call_ring);
+        TextView fakeNumber = (TextView) findViewById(R.id.tv_number_fake_call_ring);
 
         String callNumber = getContactNumber();
         String callName = getContactName();
@@ -236,89 +222,10 @@ public class FakeCallRingerActivity extends AppCompatActivity {
         fakeName.setText(callName);
         fakeNumber.setText(callNumber);
 
-//        ibCallActionButton.setOnTouchListener(new View.OnTouchListener() {
-//
-//            float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-//
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                int a = event.getAction();
-//
-//                if (a == MotionEvent.ACTION_DOWN) {
-//                    x1 = event.getX();
-//                    y1 = event.getY();
-//                    ivRing.startAnimation(ringExpandAnimation);
-//                    ibAnswer.setVisibility(View.VISIBLE);
-//                    ibDecline.setVisibility(View.VISIBLE);
-//                    ibText.setVisibility(View.VISIBLE);
-//                    ibCallActionButton.setVisibility(View.INVISIBLE);
-//                } else if (a == MotionEvent.ACTION_MOVE) {
-//                    x2 = event.getX();
-//                    y2 = event.getY();
-//
-//                    if ((x2 - 200) > x1) {
-//                        callActionButtonsLayout.removeView(callActionButtonsLayout);
-//                        callActionButtonsLayout.removeView(ivRing);
-//                        callActionButtonsLayout.removeView(ibAnswer);
-//                        callActionButtonsLayout.removeView(ibDecline);
-//                        callActionButtonsLayout.removeView(ibText);
-//                        audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-//                        callStatus.setText("");
-//
-//                        stopRinging();
-//                        mainLayout.setBackground(getResources().getDrawable(R.mipmap.answered_bg));
-//                        ibEndCall.setVisibility(View.VISIBLE);
-//                        wakeLock.acquire();
-//                        handler.postDelayed(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                long min = (secs % 3600) / 60;
-//                                long seconds = secs % 60;
-//
-//                                String dur = String.format(Locale.US, "%02d:%02d", min, seconds);
-//                                secs++;
-//                                callDuration.setText(dur);
-//                                handler.postDelayed(this, 1000);
-//
-//                            }
-//                        }, 10);
-//
-//                        handler.postDelayed(new Runnable() {
-//
-//                            @Override
-//                            public void run() {
-//                                finish();
-//                            }
-//
-//                        }, duration * 1000);
-//
-//                    } else if ((x2 + 200) < x1) {
-//                        finish();
-//                    } else if ((y2 + 200) < y1) {
-//                        finish();
-//                    } else if ((y2 - 200) > y1) {
-//                        finish();
-//                    }
-//
-//                } else if (a == MotionEvent.ACTION_UP || a == MotionEvent.ACTION_CANCEL) {
-//
-//                    ibAnswer.setVisibility(View.INVISIBLE);
-//                    ibDecline.setVisibility(View.INVISIBLE);
-//                    ibText.setVisibility(View.INVISIBLE);
-//                    ivRing.startAnimation(ringShrinkAnimation);
-//                    ibCallActionButton.setVisibility(View.VISIBLE);
-//                }
-//
-//                return false;
-//
-//            }
-//        });
-
         Animation animCallStatusPulse = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.call_status_pulse);
 
         callStatus.startAnimation(animCallStatusPulse);
 
-        //callNumber = PhoneNumberUtils.formatNumber(callNumber, "ET");
         Uri ringtoneURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
 
         ringtone = RingtoneManager.getRingtone(getApplicationContext(), ringtoneURI);
@@ -330,22 +237,22 @@ public class FakeCallRingerActivity extends AppCompatActivity {
 
     }
 
-    private String getContactNumber(){
+    private String getContactNumber() {
         String contact = null;
         Intent myIntent = getIntent();
         Bundle mIntent = myIntent.getExtras();
-        if(mIntent != null){
-            contact  = mIntent.getString(FakeCallFragment.FAKE_NUMBER);
+        if (mIntent != null) {
+            contact = mIntent.getString(FakeCallFragment.FAKE_NUMBER);
         }
         return contact;
     }
 
-    private String getContactName(){
+    private String getContactName() {
         String contactName = null;
         Intent myIntent = getIntent();
         Bundle mIntent = myIntent.getExtras();
-        if(mIntent != null){
-            contactName  = mIntent.getString(FakeCallFragment.FAKE_NAME);
+        if (mIntent != null) {
+            contactName = mIntent.getString(FakeCallFragment.FAKE_NAME);
         }
         return contactName;
     }
@@ -365,18 +272,14 @@ public class FakeCallRingerActivity extends AppCompatActivity {
     }
 
     private void stopVoice() {
-
         if (voicePlayer != null && voicePlayer.isPlaying()) {
             voicePlayer.stop();
         }
-
     }
 
     private void stopRinging() {
-
         vibrator.cancel();
         ringtone.stop();
-
     }
 
     // adds a missed call to the log and shows a notification
@@ -398,7 +301,7 @@ public class FakeCallRingerActivity extends AppCompatActivity {
         showCallLog.setType(CallLog.Calls.CONTENT_TYPE);
         notificationManager.notify(MISSED_CALL_NOTIFICATION, nBuilder.build());
 
-         CallLogUntilities.addCallToLog(contentResolver, callNumber, 0, CallLog.Calls.MISSED_TYPE, System.currentTimeMillis(),getApplicationContext());
+        CallLogUntilities.addCallToLog(contentResolver, callNumber, 0, CallLog.Calls.MISSED_TYPE, System.currentTimeMillis(), getApplicationContext());
 
     }
 
@@ -409,7 +312,6 @@ public class FakeCallRingerActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-
         super.onDestroy();
 
         stopVoice();
@@ -428,14 +330,12 @@ public class FakeCallRingerActivity extends AppCompatActivity {
         stopRinging();
         unMuteAll();
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentMediaVolume, 0);
-
     }
 
     @Override
     public void onBackPressed() {
-
+        // Do nothing
     }
-
 
 
 }
